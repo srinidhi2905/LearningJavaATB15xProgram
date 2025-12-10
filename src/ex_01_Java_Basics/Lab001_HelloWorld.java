1536cbf147b7ee4b254ae13bbc73e8baf649d4cb
Srinidhi <srinidhi2905@gmail.com>
2025-12-10 23:12:58 +0530

    Updated code

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -1,4 +1,12 @@
 package ex_01_Java_Basics;
 
 public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        String a = "Hello ";
+        String b = "World";
+        String c = a + b;
+        System.out.println(c);
+
+
+    }
 }
